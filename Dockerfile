f6941507fb8820348ea786190214d6618dba3310
Nikki Eggleton <nikkieggleton@nikkis-air.home>
2025-02-02 21:45:34 +0000

    chore: docker stuff, doesn't yet work in docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,24 @@
-FROM python:slim-bullseye
+FROM python:3.10.16-bullseye
 
 WORKDIR /app
 
-COPY as_flow.py as_local.py geoguessr_session.py signin.py requirements.txt /app/
+COPY pyproject.toml uv.lock as_flow.py as_local.py geoguessr_session.py signin.py /app/
 
-RUN pip install -r requirements.txt
+RUN apt-get update &&  \
+    apt install -y     \
+          chromium         \
+          chromium-driver  \
+          chromium-sandbox \
+          xvfb             \
+          x11vnc \
+          fluxbox \
+          xterm \
+          libffi-dev \
+          git \
+          ca-certificates
+
+ENV CONTAINERISED="true"
+
+RUN pip install uv && \
+    uv export > requirements.txt && \
+    pip install -r requirements.txt
--- signin.py
+++ signin.py
@@ -1,33 +1,39 @@
 """ Handles sign in to Geoguessr, gets the ncfa token for future calls
 which will be done via API"""
 import json
+import os
 from time import sleep
 
 from selenium.webdriver.common.by import By
-from seleniumbase import Driver
+from seleniumbase import SB
 
 
 def signin(user: str, password: str) -> dict[str, str]:
     """ Do sign in and return ncfa token"""
-    driver = Driver(uc=True, headless=False)
-    driver.uc_open_with_reconnect("https://www.geoguessr.com/signin", reconnect_time=10)
+    with SB(uc=True, xvfb=True if os.getenv("CONTAINERISED") == "true" else False) as driver:
+        driver.uc_open_with_reconnect("https://nowsecure.nl", reconnect_time=6)
+        driver.save_screenshot("/app/screeenshots/wtf3.png")
 
-    try:
-        driver.find_element(By.ID, "accept-choices").click()
-    except:
-        pass
+        driver.uc_open_with_reconnect("https://www.geoguessr.com/signin", reconnect_time=6)
 
-    driver.find_element(By.NAME, 'email').send_keys(user)
-    driver.find_element(By.NAME, 'password').send_keys(password)
-    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        try:
+            driver.find_element(By.ID, "accept-choices").click()
+        except:
+            pass
 
-    sleep(2)
+        driver.find_element(By.NAME, 'email').send_keys(user)
+        driver.find_element(By.NAME, 'password').send_keys(password)
+        driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-    ncfas = [cookie for cookie in driver.get_cookies() if cookie["name"] == "_ncfa"]
+        driver.save_screenshot("/app/screeenshots/wtf2.png")
 
-    next_data = json.loads(driver.find_element(By.ID, "__NEXT_DATA__").get_attribute("innerHTML"))
+        sleep(2)
 
-    return {
-        "token": ncfas[0]["value"],
-        "build_id": next_data["buildId"]
-    }
+        ncfas = [cookie for cookie in driver.get_cookies() if cookie["name"] == "_ncfa"]
+
+        next_data = json.loads(driver.find_element(By.ID, "__NEXT_DATA__").get_attribute("innerHTML"))
+
+        return {
+            "token": ncfas[0]["value"],
+            "build_id": next_data["buildId"]
+        }
